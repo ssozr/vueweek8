29f3bf9cb655f6346bba21600c181a7c939b5eba
ssozr <a917382460@gmail.com>
2023-02-27 22:53:44 +0800

    deploy

---
+++ assets/AboutView-e49f55b1.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as s,a as o}from"./index-b7266529.js";const _={},c={class:"about"},a=o("h1",{class:"btn"},"This is an about page",-1),n=[a];function i(r,d){return t(),s("div",c,n)}const l=e(_,[["render",i]]);export{l as default};
