3d255b1d4f139fdab39ee8dd0a28ad18128e44d8
ssozr <a917382460@gmail.com>
2023-03-05 17:40:54 +0800

    更新3

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import App from './App.vue'
 import router from './router'
+import Swiper from 'swiper'
 
 import './assets/all.scss'
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -83,33 +83,17 @@
   </div>
   <div class="container mt-30 mb-30">
     <div class="row">
-      <h2 class="text-center">陪伴無數朋友們，重新認識自己</h2>
-    </div>
-    <div class="row">
-      <div class="row justify-content-center">
-        <div class="col-8">
-          <div class="card mb-3">
-            <div class="row g-0">
-              <div class="col-4">
-                <img src="" class="img-fluid rounded-start" alt="...">
-              </div>
-              <div class="col-8">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <swiper></swiper>
     </div>
   </div>
+
 </template>
 
 <script>
+import swiper from '../components/Swiper.vue'
 export default {
-
+  components: {
+    swiper
+  }
 }
 </script>
