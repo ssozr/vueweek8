4b417087270d9e0b10feaa24823c8e08eb18c226
ssozr <a917382460@gmail.com>
2023-03-16 20:14:05 +0800

    316更新5

--- src/views/FrontLayout.vue
+++ src/views/FrontLayout.vue
@@ -5,7 +5,7 @@
         <div class="header row">
         <div class="d-flex justify-content-between py-4 m-0 px-0">
           <div>
-            <RouterLink  to="/" @click="changeBtn()"><img h-24 src="https://s3-alpha-sig.figma.com/img/c808/7901/34a7249bffdfebe8fca256378bad1a3e?Expires=1678665600&Signature=VsPOZUuQP70-5ZMFIs6MfeHIY~OtNV701HU5eIwArSWPfhJL3KFdgwOR5Jy4ver~fKF5dHdMahBUmPXohZMnJ05u6tFWLJlYUWF60oz~ln8tQrvova9RFzRIO5FzMI9cd8X2KqQSak8fAgmoobDANqP6b~xML7Xa~ATvVJTGLXwGpIAir6fz1-1DxK1s1XnqET3SIpGIkaaYgayKQG~Zc4KcrnrY4TRUn8mgy6dAs-6wfnQ~Bj22O7vd6R9ImiQn4plRdCu0unPXGVkoR~XWunm3no9KMU8SrB8ldIrDu8Z8VeUGRyadGaMEMIE7m8JfYRXcq2pEJNoFWAW~i72XtQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt=""></RouterLink>
+            <RouterLink  to="/" @click="changeBtn()"><img h-24 src="../assets/images/正視心靈.png" alt=""></RouterLink>
           </div>
             <button type="button" class="btn btn-white d-md-none" @click="menuByn()">
               <span class="material-symbols-outlined">
@@ -45,7 +45,7 @@
       <div class="container footer " v-if="!menuContent">
         <div class="row">
           <div class="text-center">
-            <div class="mb-17"><img src="https://s3-alpha-sig.figma.com/img/c808/7901/34a7249bffdfebe8fca256378bad1a3e?Expires=1678665600&Signature=VsPOZUuQP70-5ZMFIs6MfeHIY~OtNV701HU5eIwArSWPfhJL3KFdgwOR5Jy4ver~fKF5dHdMahBUmPXohZMnJ05u6tFWLJlYUWF60oz~ln8tQrvova9RFzRIO5FzMI9cd8X2KqQSak8fAgmoobDANqP6b~xML7Xa~ATvVJTGLXwGpIAir6fz1-1DxK1s1XnqET3SIpGIkaaYgayKQG~Zc4KcrnrY4TRUn8mgy6dAs-6wfnQ~Bj22O7vd6R9ImiQn4plRdCu0unPXGVkoR~XWunm3no9KMU8SrB8ldIrDu8Z8VeUGRyadGaMEMIE7m8JfYRXcq2pEJNoFWAW~i72XtQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt=""></div>
+            <div class="mb-17"><img src="../assets/images/正視心靈.png" alt=""></div>
             <div class="d-flex justify-content-center mb-2">
               <div class="img-line"></div>
             </div>
